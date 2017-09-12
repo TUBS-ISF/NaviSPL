fbca0ce2af2d7e1d05694e09846f2f6140a2f54f
tpspldev4 <tpspldev4@wks21.isf.cs.tu-bs.de>
2017-09-12 13:44:21 +0200

    Added sidebar feature from dev branch. Added delta for favorites.

--- de.tu_bs.cs.isf.jxmapviewer.spl/src/de/tu_bs/cs/isf/navi/NaviApp.java
+++ de.tu_bs.cs.isf.jxmapviewer.spl/src/de/tu_bs/cs/isf/navi/NaviApp.java
@@ -1,27 +1,39 @@
 package de.tu_bs.cs.isf.navi;
 
-import java.awt.BorderLayout;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.BoxLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.HashMap;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 
+import com.gg.slider.SideBar;
+import com.gg.slider.SidebarSection;
+import com.gg.slider.SideBar.SideBarMode;
+
+@SuppressWarnings("serial")
 public class NaviApp extends JFrame {
 
-	private static final long serialVersionUID = 7703210673451235840L;
-	private JMapViewer mapViewer;
+	protected JMapViewer mapViewer;
 	
-	private JPanel menuPanel;
+	private SideBar menuPanel;
 	
-	private List<JPanel> menuPanelList;
+	private JSplitPane splitPane;
+	
+	protected HashMap<String, JComponent> menuPanelList;
+
+	private int width = 1200;
+
+	private int height = 1000;
+	
+	private static final String VERSION = "1.0.0";
 
 	public NaviApp() {
-		super("Navi");
+		super("Navigation System "+VERSION);
 		
 		setupWindow();
 		setupGUI();
@@ -31,9 +43,23 @@ public class NaviApp extends JFrame {
 	
 	public void setupWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new BorderLayout()); 
+		setSize(width, height);
+		
+		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		splitPane.setOneTouchExpandable(true);
+		splitPane.setDividerLocation((this.getWidth()/4)*3);
 
-		setSize(1000, 1000);
+		setLayout(new GridBagLayout()); 
+		
+		GridBagConstraints constr = new GridBagConstraints();
+		constr.fill = GridBagConstraints.BOTH;
+		constr.gridx = 0;
+		constr.gridy = 0;
+		constr.weightx = 1;
+		constr.weighty = 1;
+		
+		getContentPane().add(splitPane,constr);
+
 	}
 	
 	public void setupGUI() {
@@ -43,19 +69,17 @@ public class NaviApp extends JFrame {
 	
 	public void setupMap() {
 		mapViewer = new JMapViewer();
-		add(mapViewer, BorderLayout.CENTER);
+		mapViewer.setMinimumSize(new Dimension(0, 0));
+		splitPane.setLeftComponent(mapViewer);
 	}
 	
 	public void setupMenu() {
 		
-		menuPanel = new JPanel();
-		menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.PAGE_AXIS));
-		getContentPane().add(menuPanel, BorderLayout.EAST);
-
-		JLabel menuLabel = new JLabel("-- MENU --");		
-	    menuPanel.add(menuLabel);		
+		menuPanel = new SideBar(SideBarMode.TOP_LEVEL, true, 300, true);
+		menuPanel.setMinimumSize(new Dimension(0, 0));
+		splitPane.setRightComponent(menuPanel);
 		
-		menuPanelList = new ArrayList<JPanel>();
+		menuPanelList = new HashMap<String,JComponent>();
 		
 		// Call menu functions
 		addMenu1();
@@ -74,9 +98,10 @@ public class NaviApp extends JFrame {
 		addMenu14();
 		addMenu15();		
 		
-		for (JPanel p : menuPanelList) {
-			menuPanel.add(p);
+		for (String featureName : menuPanelList.keySet()) {
+			menuPanel.add(new SidebarSection(menuPanel,featureName , menuPanelList.get(featureName)));
 		}
+	
 	}
 
 	public void addMenu1() {}
