d0f7b0eb627e161a003d7ac562a16bad4495fbf9
tpspldev2 <tpspldev2@wks19.isf.cs.tu-bs.de>
2017-09-19 16:50:01 +0200

    NaviApp.java

--- de.tu_bs.cs.isf.jxmapviewer.spl/src/de/tu_bs/cs/isf/navi/NaviApp.java
+++ de.tu_bs.cs.isf.jxmapviewer.spl/src/de/tu_bs/cs/isf/navi/NaviApp.java
@@ -4,11 +4,10 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.HashMap;
+
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JSplitPane;
-
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 
 import com.gg.slider.SideBar;
@@ -18,70 +17,70 @@ import com.gg.slider.SideBar.SideBarMode;
 @SuppressWarnings("serial")
 public class NaviApp extends JFrame {
 
-	protected JMapViewer mapViewer;
-	
+	private JMapViewer mapViewer;
+
 	private SideBar menuPanel;
-	
+
 	private JSplitPane splitPane;
-	
-	protected HashMap<String, JComponent> menuPanelList;
+
+	private HashMap<String, JComponent> menuPanelList;
 
 	private int width = 1200;
 
 	private int height = 1000;
-	
+
 	private static final String VERSION = "1.0.0";
 
 	public NaviApp() {
-		super("Navigation System "+VERSION);
-		
+		super("Navigation System " + VERSION);
+
 		setupWindow();
 		setupGUI();
 
 		setVisible(true);
 	}
-	
+
 	public void setupWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(width, height);
-		
+
 		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
 		splitPane.setOneTouchExpandable(true);
-		splitPane.setDividerLocation((this.getWidth()/4)*3);
+		splitPane.setDividerLocation((this.getWidth() / 4) * 3);
 
-		setLayout(new GridBagLayout()); 
-		
+		setLayout(new GridBagLayout());
+
 		GridBagConstraints constr = new GridBagConstraints();
 		constr.fill = GridBagConstraints.BOTH;
 		constr.gridx = 0;
 		constr.gridy = 0;
 		constr.weightx = 1;
 		constr.weighty = 1;
-		
-		getContentPane().add(splitPane,constr);
+
+		getContentPane().add(splitPane, constr);
 
 	}
-	
+
 	public void setupGUI() {
 		setupMap();
 		setupMenu();
 	}
-	
+
 	public void setupMap() {
 		mapViewer = new JMapViewer();
 		mapViewer.setMinimumSize(new Dimension(0, 0));
 		splitPane.setLeftComponent(mapViewer);
 	}
-	
+
 	public void setupMenu() {
-		
+
 		menuPanel = new SideBar(SideBarMode.TOP_LEVEL, true, 300, true);
 		menuPanel.setMinimumSize(new Dimension(0, 0));
 		splitPane.setRightComponent(menuPanel);
-		
-		menuPanelList = new HashMap<String,JComponent>();
-		
-		// Call menu functions
+
+		menuPanelList = new HashMap<String, JComponent>();
+
+		// Call menu functionsconSartCoordBtn
 		addMenu1();
 		addMenu2();
 		addMenu3();
@@ -96,30 +95,58 @@ public class NaviApp extends JFrame {
 		addMenu12();
 		addMenu13();
 		addMenu14();
-		addMenu15();		
-		
+		addMenu15();
+
 		for (String featureName : menuPanelList.keySet()) {
-			menuPanel.add(new SidebarSection(menuPanel,featureName , menuPanelList.get(featureName)));
+			menuPanel.add(new SidebarSection(menuPanel, featureName, menuPanelList.get(featureName)));
 		}
-	
 	}
 
-	public void addMenu1() {}
-	public void addMenu2() {}
-	public void addMenu3() {}
-	public void addMenu4() {}
-	public void addMenu5() {}
-	public void addMenu6() {}
-	public void addMenu7() {}
-	public void addMenu8() {}
-	public void addMenu9() {}
-	public void addMenu10() {}
-	public void addMenu11() {}
-	public void addMenu12() {}
-	public void addMenu13() {}
-	public void addMenu14() {}
-	public void addMenu15() {}
-	 
+	public void addMenu1() {
+	}
+
+	public void addMenu2() {
+	}
+
+	public void addMenu3() {
+	}
+
+	public void addMenu4() {
+	}
+
+	public void addMenu5() {
+	}
+
+	public void addMenu6() {
+	}
+
+	public void addMenu7() {
+	}
+
+	public void addMenu8() {
+	}
+
+	public void addMenu9() {
+	}
+
+	public void addMenu10() {
+	}
+
+	public void addMenu11() {
+	}
+
+	public void addMenu12() {
+	}
+
+	public void addMenu13() {
+	}
+
+	public void addMenu14() {
+	}
+
+	public void addMenu15() {
+	}
+
 	public static void main(String[] args) {
 		new NaviApp();
 	}
